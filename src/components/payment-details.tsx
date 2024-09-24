4f655b786e54dda8fff2148a992c6b31de76257f
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2024-09-24 18:57:08 -0300

    feat: create the PaymentDetails component

---
+++ src/components/payment-details.tsx
@@ -0,0 +1,61 @@
+import { Separator } from "@radix-ui/react-separator";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "./ui/card";
+
+interface PaymentMethod {
+    type?: string
+    brand: string
+    amount: number
+    installments: string
+    expiresAt: string
+    number: string
+}
+
+interface PaymentDetailsProps {
+    subTotal: number
+    freightCosts: number
+    discount: number
+    total: number
+    paymentMethod: PaymentMethod
+}
+
+export function PaymentDetails({ subTotal, freightCosts, discount, total, ...props }: PaymentDetailsProps) {
+    const { brand, number, expiresAt, installments, amount } = props.paymentMethod
+    return (
+        <Card className="shadow-lg">
+            <CardHeader>
+                <CardTitle className="text-lg font-semibold text-zinc-900">Dados do Pagamento</CardTitle>
+            </CardHeader>
+            <CardContent className="space-y-9">
+                <div className="space-y-3">
+                    <div className="flex justify-between">
+                        <p className="text-zinc-800">Subtotal</p>
+                        <p className="text-sm">R$ {subTotal}</p>
+                    </div>
+                    <div className="flex justify-between">
+                        <p className="text-zinc-800">Frete</p>
+                        <p className="text-sm">R$ {freightCosts}</p>
+                    </div>
+                    <div className="flex justify-between">
+                        <p className="text-zinc-800">Desconto</p>
+                        <p className="text-sm text-red-500">- R$ {discount}</p>
+                    </div>
+                </div>
+                <div className="flex justify-between">
+                    <p className="text-zinc-800">Valor total</p>
+                    <p className="text-sm text-lime-600">R$ {total}</p>
+                </div>
+            </CardContent>
+            <CardFooter className="flex flex-col">
+                <Separator className="h-0.5" />
+
+                <div className="flex flex-wrap justify-between items-end w-full mt-3 lg:gap-28">
+                    <div className="space-y-1">
+                        <p className="text-zinc-400">Método de Pagamento</p>
+                        <p className="text-zinc-800 text-sm">{brand} {number} Exp. {expiresAt}</p>
+                    </div>
+                    <p className="text-sm">{installments}x de R${amount}</p>
+                </div>
+            </CardFooter>
+        </Card>
+    )
+}
