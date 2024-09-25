d62d6181d8440b5fdbcd6e8b44c59f33240c79b3
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2024-09-25 10:37:14 -0300

    feat: create the OrderDetails component

---
+++ src/components/order-details.tsx
@@ -0,0 +1,31 @@
+import { formatDate } from "@/utils/fomat-date";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+
+interface OrderDetailsProps {
+    pointOfSale: string
+    purchasedIn: string
+}
+
+export function OrderDetails({ purchasedIn, pointOfSale }: OrderDetailsProps) {
+    return (
+        <Card className="shadow-lg">
+            <CardHeader>
+                <CardTitle className="text-lg font-semibold text-zinc-900">Dados do Pedido</CardTitle>
+            </CardHeader>
+            <CardContent className="space-y-3 lg:flex lg:justify-start lg:gap-20 lg:items-center lg:space-y-0">
+                <div className="flex flex-col">
+                    <p className="text-zinc-400">Comprado em</p>
+                    <p className="text-sm">{formatDate(purchasedIn)}</p>
+                </div>
+                <div className="flex flex-col">
+                    <p className="text-zinc-400">Ponto de Venda</p>
+                    <p className="text-sm">{pointOfSale}</p>
+                </div>
+                <div className="flex flex-col">
+                    <p className="text-zinc-400">Modalidade de Entrega</p>
+                    <p className="text-sm">F1 Envio pela loja, F2 Retira em Loja</p>
+                </div>
+            </CardContent>
+        </Card>
+    )
+}
