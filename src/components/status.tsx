e56519539b4792c23b43802b0dad574880e36c88
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2024-09-27 11:16:23 -0300

    fix: fix the color display of the Status component

--- src/components/status.tsx
+++ src/components/status.tsx
@@ -2,31 +2,30 @@ interface StatusProps {
     status?: 'PENDING' | 'DELIVERED' | 'SEPARATION'
 }
 
-export function Status({ status = 'PENDING' }: StatusProps) {
-
+export function Status({ status = 'PENDING'}: StatusProps) {
     let statusMsg = undefined
     let color = undefined
 
     switch (status) {
         case 'PENDING':
-            color = 'yellow'
+            color = 'bg-yellow-500 border border-yellow-600'
             statusMsg = 'Pendente'
             break;
         case 'DELIVERED':
-            color = 'lime'
+            color = 'bg-lime-500 border border-lime-600'
             statusMsg = 'Entregue'
             break;
         case 'SEPARATION':
-            color = 'cyan'
+            color = 'bg-cyan-500 border border-cyan-600'
             statusMsg = 'Separação'
             break;
         default:
             break;
     }
-
+    
     return (
         <div className="flex justify-start items-center gap-2">
-            <span className={`inline-block rounded-full w-4 h-4 bg-${color}-500 border border-${color}-600`}></span>
+            <span className={`inline-block rounded-full w-4 h-4 ${color}`}></span>
             <p className="text-sm text-zinc-500">{statusMsg}</p>
         </div>
     )
