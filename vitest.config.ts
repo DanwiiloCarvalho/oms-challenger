13cb102c0dc5d78e2287b7d0e0dcbd32e7c32f2f
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2025-02-04 18:41:03 -0300

    chore: configure Vitest

---
+++ vitest.config.ts
@@ -0,0 +1,25 @@
+/// <reference types="vitest" />
+
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    plugins: [react()],
+
+    test: {
+        globals: true,
+        environment: "jsdom",
+        setupFiles: ["./src/setupTests.ts"],
+        include: ["src/**/*.spec.tsx", "src/**/*.spec.ts"],
+        coverage: {
+            exclude: [
+                "*/types/*",
+                "src/main.tsx",
+            ]
+        }
+    },
+    resolve: {
+        alias: [{ find: "@", replacement: path.resolve(__dirname, "./src") }],
+    },
+});
